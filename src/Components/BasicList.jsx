2580ff41d182aa832bafe779f8b79440b9129150
Gaby Küpfer <gaby@itsme3d.com>
2019-08-29 19:47:52 -0400

    Refactored styles

--- src/Components/BasicList.jsx
+++ src/Components/BasicList.jsx
@@ -7,7 +7,7 @@ function BasicList({ data, title }) {
       <h2>{title}</h2>
       <ul>
         {data.map(item => {
-          return <li className={styles.listItem}>
+          return <li className={styles.listItem} key={item.name}>
             <a href="www.fake-link-to-pass-tests-on-ci.com" className={styles.link}>
               {item.name}
             </a>
