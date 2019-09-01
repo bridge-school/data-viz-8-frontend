6c7bebf52ecd36ace8e9b5fb9c6d9f36bfc7ec91
Gaby Küpfer <33757394+gabycodes@users.noreply.github.com>
2019-08-31 23:09:23 -0400

    Task/Refactor styles with scss and css modules (#2)
    * added node-sass, refactored styles
    
    * bit more style refactoring
    
    * moved component-specific styles to css modules
    
    * moved component-specific styles to css modules
    
    * Refactored styles
    
    * Removed rando packages
    
    * Removed unused file, moved things around

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import "./App.css";
+import "./App.scss";
 import { request } from "./backend-request";
 import CohortSelectionPage from './Pages/CohortSelectionPage'
 
