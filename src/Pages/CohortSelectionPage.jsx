bb16f5f463f268b066b731c4476367a9263f89b4
Gaby Küpfer <33757394+gabycodes@users.noreply.github.com>
2019-09-02 14:21:31 -0400

    Feature/cohort type selection screen (#4)
    * added node-sass, refactored styles
    
    * bit more style refactoring
    
    * moved component-specific styles to css modules
    
    * moved component-specific styles to css modules
    
    * Refactored styles
    
    * Removed rando packages
    
    * Removed unused file, moved things around
    
    * Cohort type selection screen
    
    * removed comment

--- src/Pages/CohortSelectionPage.jsx
+++ src/Pages/CohortSelectionPage.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import BasicList from '../Components/BasicList'
+import BasicLinkList from '../Components/BasicLinkList'
 import Header from '../Components/Header'
 
-function CohortSelectionScreen() {
+function CohortSelectionPage() {
   return (
     <>
       <div className="App">
         <Header />
         <div className="wrapper">
           {/* TODO: replace this data with data from json dataset, then from the BE, eventually */}
-          <BasicList title="Frontend Development Cohorts" data={[
+          <BasicLinkList title="Frontend Development Cohorts" data={[
             { name: 'Cohort 3'},
             { name: 'Cohort 2'},
             { name: 'Cohort 1'},
@@ -20,4 +20,4 @@ function CohortSelectionScreen() {
   )
 }
 
-export default CohortSelectionScreen;
+export default CohortSelectionPage;
