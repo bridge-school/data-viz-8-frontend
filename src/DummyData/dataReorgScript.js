02a4f4d3e0c8357bb3d7d199bf5fb20658b15b20
Caitlin Haaf <caitlinhaafillustration@gmail.com>
2019-09-08 18:28:48 -0400

    Dummy data now applied to CohortDetailsPage (minority breakdown only). Cleared missing key error message for BasicButtonList component.

--- src/DummyData/dataReorgScript.js
+++ src/DummyData/dataReorgScript.js
@@ -1,23 +1,6 @@
 var fs = require('fs');
 var applicants = require('./cohortDataRaw');
 
-// example applicant:
-// {
-//     "cohort": "cohort-7",
-//     "shortlisted": "yes",
-//     "applicantID": "3803e908-1c1d-43a6-a485-55a82d7f5fbb",
-//     "gender": "Woman",
-//     "pronouns": "She/her",
-//     "identities": "Person of colour",
-//     "heardAbout": "Event",
-//     "previouslyApplied": "I have not applied to a Bridge cohort before",
-//     "employmentStatus": "Employed full time",
-//     "seekingJob": "Yes",
-//     "bootcamps": ", Bloc",
-//     "submittedAt": "2/12/2019 5:24:49",
-//     "token": "f59aa4ef301706499d6ed87697e562bc"
-//   }
-
 var dummyCohortData = applicants.reduce( (accumulator, applicant) => {
     // sort applicants by cohort number
     if(accumulator[applicant.cohort]===undefined){
@@ -29,7 +12,7 @@ var dummyCohortData = applicants.reduce( (accumulator, applicant) => {
     }
     // split identities into array
     applicant.identities = applicant.identities.split(", ")
-    // split previously applied into arrary?
+    // split previously applied into arrary
     applicant.previouslyApplied = applicant.previouslyApplied.split(", ")
     // split bootcamps into array
     applicant.bootcamps = applicant.bootcamps.split(", ")
