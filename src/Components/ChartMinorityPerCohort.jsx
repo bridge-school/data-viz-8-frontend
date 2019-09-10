ced2e168944f796846f33001b11682614611ec95
Caitlin Haaf <caitlinhaafillustration@gmail.com>
2019-09-10 19:56:25 -0400

    Applied VictoryChart theme to bar graph component. (#13)
    * Applied VictoryChart theme to bar graph component.
    Removed "no response" data set from minority graph data.
    Cleared react unique key error from BasicLinkList component
    
    * Reversed position of graph and cohort selection list on CohortSelectionPage per the PO feedback.

--- src/Components/ChartMinorityPerCohort.jsx
+++ src/Components/ChartMinorityPerCohort.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { VictoryChart, VictoryBar } from 'victory';
+import materialTheme from '../Styles/victoryMaterialTheme';
 
 function ChartMinorityPerCohort({ data }) {
     return (
         <VictoryChart
-            // TODO: come up with our own Victory theme?
-            // theme={VictoryTheme.material}
-            domainPadding={10}
+            theme={materialTheme}
+            domainPadding={{ x:[20, 12] }}
+            animate={{
+                duration: 1200,
+                onLoad: { duration: 1000 }
+                }} 
         >
             <VictoryBar
                 style={{ data: { fill: "#4E57CA" } }}
