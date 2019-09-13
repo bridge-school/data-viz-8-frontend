f56c242964e5ef6838f8bf22f032feca18494f31
Gaby Küpfer <33757394+gabycodes@users.noreply.github.com>
2019-09-12 20:11:02 -0400

    Update bar graph styles (non-responsive) (#16)
    * stylezz
    
    * FINALLY the label angle thing works
    
    * Finished reworking bar graph! Non-responsive
    
    * Removed unused variable
    
    * refactor

--- src/Components/ChartMinorityPerCohort.jsx
+++ src/Components/ChartMinorityPerCohort.jsx
@@ -1,21 +1,54 @@
 import React from "react";
-import { VictoryChart, VictoryBar } from 'victory';
+import { VictoryChart, VictoryBar, VictoryLabel, VictoryAxis } from 'victory';
 import materialTheme from '../Styles/victoryMaterialTheme';
 
 function ChartMinorityPerCohort({ data }) {
+    const findResponsePercentage = (numberOfResponses) => {
+        // TODO: replace hardcoded value with number from api
+        const totalNumberOfApplicants = 135
+        return `${Math.round(numberOfResponses/totalNumberOfApplicants*100)}%`
+    }
+
+    const maxDomainValue =
+        data
+            .map(item => item.y)
+            .reduce((currentMax, currentItem) => Math.max(currentMax, currentItem))
+            + 10
+    
     return (
         <VictoryChart
             theme={materialTheme}
-            domainPadding={{ x:[20, 12] }}
+            domainPadding={{ x: [20, 18], y: [10, maxDomainValue] }}
             animate={{
-                duration: 1200,
-                onLoad: { duration: 1000 }
-                }} 
+                onLoad: { duration: 200 }
+            }} 
+            style={{
+                parent: {
+                    position: 'relative',
+                    top: '-70px',
+                    left: 50,
+                    height: 220
+                }, 
+            }}
+            height={220}
         >
-            <VictoryBar
-                style={{ data: { fill: "#4E57CA" } }}
-                data={data}
-            />
+        <VictoryAxis
+            tickLabelComponent={<VictoryLabel angle={45} textAnchor='start' />}
+        />
+        <VictoryAxis 
+            dependentAxis
+            theme={materialTheme}
+            label='Number of Applicants'
+            style={{ axisLabel: { fontSize: 6, padding: 30 }} }
+        />
+        <VictoryBar
+            data={data}
+            labels={({ datum }) => findResponsePercentage(datum.y)}
+            style={{
+                data: { fill: "#4E57CA" },
+            }}
+            labelComponent={ <VictoryLabel dy={-2}/> }
+        />
         </VictoryChart>
     );
 }
