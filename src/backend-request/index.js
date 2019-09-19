822390915d208bec1ca8fb0a6caec1c6920acf01
Gaby Küpfer <gaby@itsme3d.com>
2019-09-19 11:18:10 -0400

    Set api-base-url back to our deployed app address

--- src/backend-request/index.js
+++ src/backend-request/index.js
@@ -1,7 +1,6 @@
-const API_BASE_URL = 'http://localhost:8081'
-  // process.env.NODE_ENV === "development"
-  //   ? ""
-  //   : `http://${process.env.REACT_APP_PROJECT_NAME}-backend.bridgeschoolapp.io`;
+const API_BASE_URL = process.env.NODE_ENV === "development"
+    ? ""
+    : `http://${process.env.REACT_APP_PROJECT_NAME}-backend.bridgeschoolapp.io`;
 
 export const request = (endpoint, method = "GET", body) =>
   fetch(`${API_BASE_URL}/${endpoint}`, {
